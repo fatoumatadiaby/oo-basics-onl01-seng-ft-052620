f9881df863d914a9f0599a7c22f2293b4db74981
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-05 22:46:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,42 @@
-# Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def brand
+    @brand
+  end
+ 
+  def color=(color)
+    @color = color
+  end
+ 
+  def color  
+    @color 
+  end
+ 
+  def size=(num)
+    @size = num
+  end
+ 
+  def size 
+    @size 
+  end
+ 
+  def material=(material)
+    @material = material
+  end
+ 
+  def material
+    @material 
+  end
+ 
+  def cobble= (cobble)
+    @cobble = cobble
+  end
+  
+  def cobble
+   @cobble
+end
+
+end   
